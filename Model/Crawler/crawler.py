9536ee8c42c39bf8d647c5db40bebc0037fdfb82
MoeinMirbaha <mirbahamoein98@gmail.com>
2023-09-07 13:40:23 +0330

    restructure

---
+++ Model/Crawler/crawler.py
@@ -0,0 +1,252 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.remote.remote_connection import LOGGER
+import time
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import warnings
+
+warnings.filterwarnings("ignore")
+LOGGER.setLevel("ERROR")
+
+#ChromeDriverManager().install()
+
+def crawl_links(url) -> set:
+    def run_driver():
+        chrome_options = Options()
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--headless")  
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(url) #https://divar.ir/s/tehran/rent-apartment?sort=sort_date # https://divar.ir/s/tehran/rent-apartment/tehran-zanjan?sort=sort_date
+        time.sleep(10)
+        return driver
+    
+    def get_cases(driver: webdriver.Chrome):
+        def scroll(driver):
+            driver.execute_script("window.scrollBy(0, 500);")
+        
+        def height(driver):
+            return driver.execute_script("return document.body.scrollHeight")
+        
+        def check_for_error(driver):
+            driver.find_element_by_css_selector(f"button.kt-button kt-button--primary kt-button--small").click()
+            time.sleep(20)
+            driver.find_element_by_css_selector(f"button.kt-button kt-button--primary kt-button--small")
+            
+        cases = set()  
+        initial_height = height(driver)
+        j = 0
+        i = 1
+        pbar = tqdm(j+1)
+        while True:
+            html = driver.page_source
+            soup = BeautifulSoup(html, features="html.parser")
+            caselist = soup.find_all("div", attrs= {"class": "post-card-item-af972 kt-col-6-bee95 kt-col-xxl-4-e9d46"})
+            for case in caselist:
+                cases.add(case)
+            scroll(driver)
+            i += 1
+            j += 1
+            pbar.update(1)
+            time.sleep(5)
+
+            current_height = height(driver)
+            if current_height == initial_height:
+                if i % 10 == 0:
+                    try:
+                        check_for_error(driver)
+                        print("reload button appeared")
+                        break
+                    except: 
+                        time.sleep(5)
+                        current_height = height(driver)
+                        if current_height == initial_height:
+                            print("height didnt change")
+                            break
+                        else:
+                            initial_height = current_height
+                            i = 1               
+            else:    
+                initial_height = current_height
+                i = 1
+        driver.quit()
+        return cases
+    
+    try:
+        driver = run_driver()
+        return get_cases(driver)
+    except:
+        return crawl_links()
+
+def get_data(cases) -> pd.DataFrame:
+    
+    
+    def get_values(case):
+        def get_url(case):
+            return "https://divar.ir" + case.a.get("href")
+        
+        def get_soup(url):
+            return BeautifulSoup(session.get(url, timeout= 10).text, features="html.parser")
+            
+        def get_title(soup):
+            return soup.find("div", attrs= {"class": "kt-page-title__title kt-page-title__title--responsive-sized"}).text
+        
+        def get_zone(soup):
+            return soup.find("div", attrs= {"class": "kt-page-title__subtitle kt-page-title__subtitle--responsive-sized"}).text
+        
+        def get_description(soup):
+            return soup.find("p", attrs= {"class": "kt-description-row__text kt-description-row__text--primary"}).text
+        
+        def get_area_year_rooms(soup):
+            area_built_rooms = soup.find_all("span", attrs= {"class": "kt-group-row-item__value"})
+            area = area_built_rooms[0].text
+            built = area_built_rooms[1].text
+            rooms = area_built_rooms[2].text
+            return area, built, rooms
+        
+        def get_rent_costs(soup):
+            rent_pre = soup.find_all("div", attrs= {"class": "convert-slider__info kt-row"})
+            if len(rent_pre) > 0:
+
+                try:
+                    pish1 = rent_pre[0].find_all("div")[0].span.text
+                except:
+                    pish1 = rent_pre[0].find_all("div")[0].text
+                try:
+                    pish2 = rent_pre[0].find_all("div")[1].span.text
+                except:
+                    pish2 = rent_pre[0].find_all("div")[1].text
+                try:
+                    rent1 = rent_pre[1].find_all("div")[0].span.text
+                except:
+                    rent1 = rent_pre[1].find_all("div")[0].text
+                try:
+                    rent2 = rent_pre[1].find_all("div")[1].span.text
+                except:
+                    rent2 = rent_pre[1].find_all("div")[1].text
+                
+                rent_pre = {"pish1": pish1, "rent1": rent1,\
+                            "pish2": pish2, "rent2": rent2}
+            else:
+                rent_pre = soup.find_all("p", attrs= {"class": "kt-unexpandable-row__value"})
+                rent_pre = {"pish1": rent_pre[0].text, "rent1": rent_pre[1].text,\
+                            "pish2": np.nan, "rent2": np.nan}
+            return rent_pre
+        
+        def get_level(soup):
+            for row in soup.find_all("div", attrs= {"class": "kt-base-row kt-base-row--large kt-unexpandable-row"}):
+                if row.find("div", attrs= {"class": "kt-base-row__start kt-unexpandable-row__title-box"}).p.text == "طبقه":
+                    tabaghe = row.find("div", attrs= {"class": "kt-base-row__end kt-unexpandable-row__value-box"}).p.text
+                    break
+            else:
+                tabaghe = np.nan
+            return tabaghe
+        def get_elev_park_ware(soup):
+            epw = soup.find_all("div", attrs= {"class": "kt-group-row"})
+            if len(epw) == 2:
+                epw = epw[1].find_all("div")
+            else:
+                epw = epw[2].find_all("div")
+            elevator = len(epw[0].get("class")) == 1
+            parking = len(epw[1].get("class")) == 1
+            warehouse = len(epw[2].get("class")) == 1
+            return elevator, parking, warehouse
+        
+        session = requests.Session()
+        try:
+            url = get_url(case)
+        except:
+            return 0
+        try:
+            soup = get_soup(url)
+            title = get_title(soup)
+        except:
+            time.sleep(10)
+            try:
+                soup = get_soup(url)
+                title = get_title(soup)
+            except:
+                return 0
+        zone = get_zone(soup)
+        description = get_description(soup)
+        area, built, rooms = get_area_year_rooms(soup)
+        rent_pre_dict = get_rent_costs(soup)
+        level = get_level(soup)
+        elevator, parking, warehouse = get_elev_park_ware(soup)
+        return title, description, zone, area, built, rooms, rent_pre_dict, level, elevator, parking, warehouse, url
+    
+    df = pd.DataFrame(columns = ["title", "description", "zone", "area", "year", "number of rooms", "pish1", "rent1", "pish2", "rent2", "level", "elevator", "parking", "warehouse", "url"])
+    for case in tqdm(cases):
+        time.sleep(2)
+        try:
+            title, description, zone, area, built, rooms, rent_pre_dict, level, elevator, parking, warehouse, url = get_values(case)
+            df.loc[len(df)] = {
+                "title": title,\
+                "description": description,\
+                "area": area,\
+                "zone": zone,\
+                "year": built,\
+                "number of rooms": rooms,\
+                "pish1": rent_pre_dict["pish1"],\
+                "rent1": rent_pre_dict["rent1"],\
+                "pish2": rent_pre_dict["pish2"],\
+                "rent2": rent_pre_dict["rent2"],\
+                "level": level,\
+                "elevator": elevator,\
+                "parking": parking,\
+                "warehouse": warehouse,\
+                "url": url
+                }
+        except:
+            pass
+    return df
+
+def clean_data(df):
+    persian_to_western = {
+                        "۰": "0", "۱": "1", "۲": "2", "۳": "3", "۴": "4",
+                        "۵": "5", "۶": "6", "۷": "7", "۸": "8", "۹": "9",
+                        }
+    free = ["مجانی", "اجاره رایگان", "توافقی"]
+    no_rooms = "بدون اتاق"
+    for i in df.index:
+        if df.loc[i, "year"][0] == 'ق':
+            df.loc[i, "year"] = 1365
+        try:
+            df.loc[i, "zone"] = df.loc[i, "zone"].split("،")[1].strip()
+        except:
+            df.loc[i, "zone"] = df.loc[i, "zone"].split(" در ")[1].strip()
+        for column in ["area", "year", "number of rooms", "pish1", "rent1", "pish2", "rent2"]:
+            try:
+                df.loc[i, column] = df.loc[i, column].replace("٬", "")
+                for persian, western in persian_to_western.items():
+                    df.loc[i, column] = df.loc[i, column].replace(persian, western)
+                splitted = df.loc[i, column].split(" ")
+                if len(splitted) > 1:
+                    if splitted[1] == "تومان":
+                        df.loc[i, column] = float(splitted[0])
+                    elif splitted[1] == "میلیون":
+                        df.loc[i, column] = float(splitted[0]) * 1000000
+                    elif splitted[1] == "هزار":
+                        df.loc[i, column] = float(splitted[0]) * 1000
+                    elif splitted[1] == "میلیارد":
+                        df.loc[i, column] = float(splitted[0]) * 1000000000
+                if df.loc[i, column] in free:
+                    df.loc[i, column] = 0
+                if df.loc[i, column] == no_rooms:
+                    df.loc[i, column] = 0    
+            except:
+                pass
+        try:
+            df.loc[i, "level"] = int(df.loc[i, "level"][0])
+        except:
+            df.loc[i, "level"] = 0
+    return df
+
+#clean_data(get_data(crawl_links())).to_excel("cases2.xlsx")
---
+++ main.py
@@ -0,0 +1,69 @@
+from Model.Crawler.crawler import crawl_links, get_data, clean_data
+import joblib
+import time
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+import tqdm
+
+fullurl = "https://divar.ir/s/tehran/rent-apartment?sort=sort_date"
+ostad_zanjan_url = "https://divar.ir/s/tehran/rent-apartment/tehran-zanjan?districts=195&sort=sort_date"
+
+def percentage_rmse(X_val, y_val, estimator, labels,
+        X_train, y_train, weight_val=None, weight_train=None,
+        config=None, groups_val=None, groups_train=None):
+    start = time.time()
+    y_pred = estimator.predict(X_val)
+    pred_time = (time.time() - start) / len(X_val)
+    val_rmsep = np.sqrt(np.mean(np.square(((y_val - y_pred) / np.minimum(y_val, y_pred)))))
+    y_pred = estimator.predict(X_train)
+    train_rmsep = np.sqrt(np.mean(np.square(((y_train - y_pred) / np.minimum(y_train, y_pred)))))
+    return val_rmsep, {
+        "val_rmsep": val_rmsep,
+        "train_rmsep": train_rmsep,
+        "pred_time": pred_time,
+    }
+
+#clean_data(get_data(crawl_links(ostad_zanjan_url))).to_excel("newcases.xlsx")
+
+automl_model = joblib.load("./Model/objs/trained_model.joblib")
+x_scaler = joblib.load("./Model/objs/x_scaler.joblib")
+X_cols = joblib.load("./Model//objs/x_columns.jobllib")
+outlier_detection_model = joblib.load("./Model//objs/outlier_model.jobllib")
+
+df = pd.read_excel("newcases.xlsx", index_col= 0)
+df["full mortgage"] = df["pish1"].astype(float) + df["rent1"].astype(float) * 100/3
+
+df = df[(df["full mortgage"] > 10000000)].reset_index(drop= True)
+
+prediction_array = pd.DataFrame(columns = X_cols)
+    
+poly = PolynomialFeatures(degree=3)
+year_poly = pd.DataFrame(poly.fit_transform(df[["year"]]), columns= ["intercept", "year", "year2", "year3"])
+area_poly = pd.DataFrame(poly.fit_transform(df[["area"]]), columns= ["intercept_area", "area", "area2", "area3"]).drop(["intercept_area"], axis = 1)
+df = pd.concat([df.drop(["year", "area"], axis = 1).reset_index(drop= True), year_poly, area_poly], axis= 1)
+
+X_df = df.drop(["full mortgage", "title", "description", "url", "number of rooms", "area3", "pish1", "pish2", "rent1", "rent2"], axis = 1)
+
+prediction_array = pd.DataFrame(columns = X_cols)
+for index in tqdm.tqdm(range(len(X_df))):
+    zone = X_df['zone'][index]
+    for col in X_cols:
+        try:
+            prediction_array.loc[index, col] = X_df[col][index]
+        except:
+            prediction_array.loc[index, col] = 0
+    prediction_array.loc[index, zone] = True
+prediction_array.fillna(0)
+
+scaled_X = x_scaler.fit_transform(prediction_array)
+auto_pred = automl_model.predict(scaled_X)
+
+df_column_order = df.columns.tolist()
+df_column_order.remove("full mortgage")
+df_column_order.append("full mortgage")
+df = df[df_column_order]
+conc = pd.concat([df, pd.Series(auto_pred)], axis= 1)
+conc.columns = conc.columns.tolist()[:-1] + ["prediction"]
+conc["error percentage"] = (conc["full mortgage"] - conc["prediction"]).abs() / pd.concat([conc["prediction"], conc["full mortgage"]], axis=1).min(axis=1) * 100
+conc.to_excel("new results.xlsx")
